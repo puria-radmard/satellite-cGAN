3340aa9d4fd5db0ed3a0edbd83acd07521cbce67
hansen7 <hc.wang96@gmail.com>
2020-10-19 13:25:06 +0100

    removed wandb logs

--- test_cGAN.py
+++ test_cGAN.py
@@ -7,17 +7,22 @@ model_weights = torch.load("saves/reg_model.epoch79.t7")["state"]
 cGAN = ConditionalGAN(["LSTN"], ["NDVI", "NDBI", "NDWI"], 0, 0)
 cGAN.load_state_dict(model_weights)
 
-image_id = '../data_source/LONDON_DATASET/gcpev16xwfth--LONDON--2020-06-25--10-58-18'
+image_id = '../data_source/LONDON_DATASET/gcpgf4du7rsv--LONDON--2018-06-29--10-51-26'
 
 imV = read_raster(f'{image_id}.NDVI.tif')[0]
 imB = read_raster(f'{image_id}.NDBI.tif')[0]
 imW = read_raster(f'{image_id}.NDWI.tif')[0]
 
 image = np.dstack([imV, imB, imW])
+image[image!=image]=0
+
 mix, miy = [int(m / 2) for m in image.shape[:2]]
 s = int(256 / 2)
 image = image[mix - s : mix + s, miy - s : miy + s]
-plt.imsave("example_index_map.png", image)
+import pdb; pdb.set_trace()
+save_image = image - np.nanmin(image)
+save_image = save_image / np.nanmax(save_image)
+plt.imsave("example_index_map.png", save_image)
 
 LSTN_real = read_raster(f'{image_id}.LSTN.tif')[0][:,:,np.newaxis]
 plt.imsave("example_LSTN_real.png", (LSTN_real[:,:,0] + 1) / 2)
