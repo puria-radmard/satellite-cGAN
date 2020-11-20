4923b3073fec48a68876bba5d5d161665ff24df8
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-11-20 00:37:13 +0000

    asdf

--- models/fpn.py
+++ models/fpn.py
@@ -4,6 +4,52 @@ from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
 # https://github.com/qubvel/segmentation_models.pytorch/
 
 
+class FPNDecoder(nn.Module):
+    def __init__(
+            self,
+            encoder_channels,
+            encoder_depth=5,
+            pyramid_channels=256,
+            segmentation_channels=128,
+            dropout=0.2,
+            merge_policy="add",
+    ):
+        super().__init__()
+
+        self.out_channels = segmentation_channels if merge_policy == "add" else segmentation_channels * 4
+        if encoder_depth < 3:
+            raise ValueError("Encoder depth for FPN decoder cannot be less than 3, got {}.".format(encoder_depth))
+
+        encoder_channels = encoder_channels[::-1]
+        encoder_channels = encoder_channels[:encoder_depth + 1]
+
+        self.p5 = nn.Conv2d(encoder_channels[0], pyramid_channels, kernel_size=1)
+        self.p4 = FPNBlock(pyramid_channels, encoder_channels[1])
+        self.p3 = FPNBlock(pyramid_channels, encoder_channels[2])
+        self.p2 = FPNBlock(pyramid_channels, encoder_channels[3])
+
+        self.seg_blocks = nn.ModuleList([
+            SegmentationBlock(pyramid_channels, segmentation_channels, n_upsamples=n_upsamples)
+            for n_upsamples in [3, 2, 1, 0]
+        ])
+
+        self.merge = MergeBlock(merge_policy)
+        self.dropout = nn.Dropout2d(p=dropout, inplace=True)
+
+    def forward(self, *features):
+        c2, c3, c4, c5 = features[-4:]
+
+        p5 = self.p5(c5)
+        p4 = self.p4(p5, c4)
+        p3 = self.p3(p4, c3)
+        p2 = self.p2(p3, c2)
+
+        feature_pyramid = [seg_block(p) for seg_block, p in zip(self.seg_blocks, [p5, p4, p3, p2])]
+        x = self.merge(feature_pyramid)
+        x = self.dropout(x)
+
+        return x
+
 class ResNetEncoder(ResNet):
     def __init__(self, encoder_name, n_channels, depth, **kwargs):
 
